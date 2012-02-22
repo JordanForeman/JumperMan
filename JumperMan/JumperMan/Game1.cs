772a6be05f0bb3f2747459d28fcc354b153a4845
Jordan Foreman <thejordanforeman@gmail.com>
2012-02-22 11:37:00 -0600

    second commit

---
+++ JumperMan/JumperMan/Game1.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace JumperMan
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Player player;
+
+        Texture2D background;
+
+        // Keyboard states
+        KeyboardState currentKeyboardState;
+        KeyboardState previousKeyboardState;
+
+        // Gamepad states
+        GamePadState currentGamePadState;
+        GamePadState previousGamePadState;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            player = new Player(this);
+            background = Content.Load<Texture2D>("background");
+            player.LoadContent(this.Content);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+
+            // Save previous states of key/gamepad presses
+            previousGamePadState = currentGamePadState;
+            previousKeyboardState = currentKeyboardState;
+
+            // Read the current states
+            currentKeyboardState = Keyboard.GetState();
+            currentGamePadState = GamePad.GetState(PlayerIndex.One);
+
+            player.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue); //Sets the default background
+
+            // Start Drawing
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(background, new Rectangle(0, 0, background.Width, background.Height), Color.White);
+
+            // Draw the Player
+            player.Draw(spriteBatch);
+            
+            // Stop drawing
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private void UpdatePlayer(GameTime gameTime)
+        {
+            
+        }
+    }
+}
---
+++ JumperMan/JumperMan/Player.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Media;
+
+namespace JumperMan
+{
+    class Player : Sprite
+    {
+        public static Game1 gameContext;
+
+        const string Asset = "player";
+        const int startX = 20;
+        const int startY = 373;
+        const int MOVE_UP = -1;
+        const int MOVE_DOWN = 1;
+        const int MOVE_LEFT = -1;
+        const int MOVE_RIGHT = 1;
+
+        public enum State { Walking, Jumping, Still }
+
+        public int Health = 3;
+        public Vector2 MoveSpeed = new Vector2(6.0f, 3.5f);
+        public State CurrentState = State.Still;
+        private Vector2 Dir = Vector2.Zero;
+
+        private Vector2 StartPosition;
+        private KeyboardState PreviousKeyboardState;
+        private int WalkState = 0;
+
+        public Player(Game1 g)
+        {
+            gameContext = g;
+        }
+
+        public void LoadContent(ContentManager theContentManager)
+        {
+            Position = new Vector2(startX, startY);
+            base.LoadContent(theContentManager, Asset);
+            Source = new Rectangle(0, 0, 28, Source.Height);
+        }
+
+        public void Update(GameTime theGameTime)
+        {
+            KeyboardState CurrentKeyboardState = Keyboard.GetState();
+            GamePadState CurrentGamepadState = GamePad.GetState(PlayerIndex.One);
+            UpdateMovement(CurrentKeyboardState, CurrentGamepadState);
+            PreviousKeyboardState = CurrentKeyboardState;
+            base.Update(theGameTime, MoveSpeed, Dir);
+        }
+
+        private void UpdateMovement(KeyboardState CurrentKeyboardState, GamePadState CurrentGamePadState)
+        {
+            //Keyboard / D-Pad controls
+            if (CurrentKeyboardState.IsKeyDown(Keys.Left) || CurrentGamePadState.DPad.Left == ButtonState.Pressed)
+            {
+                Position.X -= MoveSpeed.X;
+                CurrentState = (CurrentState != State.Jumping) ? State.Walking : CurrentState;
+            }
+            if (CurrentKeyboardState.IsKeyDown(Keys.Right) || CurrentGamePadState.DPad.Right == ButtonState.Pressed)
+            {
+                Position.X += MoveSpeed.X;
+                CurrentState = (CurrentState != State.Jumping) ? State.Walking : CurrentState;
+            }
+
+            if (CurrentGamePadState.ThumbSticks.Left.X != 0)
+            {
+                Position.X += CurrentGamePadState.ThumbSticks.Left.X * MoveSpeed.X;
+                CurrentState = (CurrentState != State.Jumping) ? State.Walking : CurrentState;
+            }
+
+            if (CurrentKeyboardState.IsKeyDown(Keys.Up) || CurrentKeyboardState.IsKeyDown(Keys.Space) || CurrentGamePadState.DPad.Up == ButtonState.Pressed || CurrentGamePadState.Buttons.A == ButtonState.Pressed)
+            {
+                UpdateJump(CurrentKeyboardState, CurrentGamePadState);
+            }
+
+            //TODO: Make crouching logic(?)
+            if (CurrentKeyboardState.IsKeyDown(Keys.Down) || CurrentGamePadState.DPad.Down == ButtonState.Pressed)
+            {
+                //CROUCH
+            }
+
+            if (!CurrentKeyboardState.IsKeyDown(Keys.Left) && !CurrentKeyboardState.IsKeyDown(Keys.Right) && CurrentGamePadState.DPad.Left != ButtonState.Pressed && CurrentGamePadState.DPad.Right != ButtonState.Pressed && CurrentGamePadState.ThumbSticks.Left.X == 0 && CurrentState == State.Walking)
+            {
+                CurrentState = State.Still;
+                Source = new Rectangle(0, 0, 28, Source.Height);
+            }
+
+            if (CurrentState == Player.State.Jumping)
+            {
+                UpdateJump(CurrentKeyboardState, CurrentGamePadState);
+            }
+
+            if (CurrentState == State.Walking)
+            {
+                int x = 0;
+                int width = 0;
+                switch (WalkState)
+                {
+                    case 0:
+                        x = 61;
+                        width = 30;
+                        WalkState += 1;
+                        break;
+                    case 1:
+                        x = 94;
+                        width = 24;
+                        WalkState += 1;
+                        break;
+                    case 2:
+                        x = 119;
+                        width = 28;
+                        WalkState += 1;
+                        break;
+                    case 3:
+                        x = 0;
+                        width = 28;
+                        WalkState = 0;
+                        break;
+                }
+                Source = new Rectangle(x, 0, width, Source.Height);
+            }
+
+            //Make sure that the player does not go out of bounds
+            Position.X = MathHelper.Clamp(Position.X, 0, gameContext.GraphicsDevice.Viewport.Width - Size.Width);
+            Position.Y = MathHelper.Clamp(Position.Y, 0, gameContext.GraphicsDevice.Viewport.Height - Size.Height);
+        }
+
+        public void UpdateJump(KeyboardState aCurrentKeyboardState, GamePadState aCurrentGamePadState)
+        {
+            if ((CurrentState == State.Walking || CurrentState == State.Still) && (aCurrentKeyboardState.IsKeyDown(Keys.Up) == true || aCurrentGamePadState.DPad.Up == ButtonState.Pressed || aCurrentGamePadState.Buttons.A == ButtonState.Pressed))
+            {
+                Jump();
+            }
+
+            if (CurrentState == State.Jumping)
+            {
+
+                if (StartPosition.Y - Position.Y > 80)  //At peak of jump...
+                { 
+                    Dir.Y = MOVE_DOWN;
+                    MoveSpeed.Y = 3.0f;
+                }
+                
+                Position.Y = (Dir.Y == MOVE_DOWN) ? Position.Y + MoveSpeed.Y : Position.Y - MoveSpeed.Y; //Change Y position
+
+                //END JUMP LOGIC
+                if (Position.Y > StartPosition.Y)
+                {
+                    Position.Y = StartPosition.Y;
+                    CurrentState = State.Still;
+                    Source = new Rectangle(0, 0, 28, Source.Height);
+                    Dir.Y = 0;
+                    MoveSpeed.X = 6.0f;
+                    MoveSpeed.Y = 3.5f;
+                }
+            }
+        }
+
+        //Start Jumping!
+        private void Jump()
+        {
+            if (CurrentState != State.Jumping)
+            {
+                CurrentState = State.Jumping;
+                Source = new Rectangle(29, 0, 31, Source.Height);
+                StartPosition = Position;
+                Dir.Y = MOVE_UP;
+                MoveSpeed.X = 3.0f;
+            }
+        }
+    }
+}
